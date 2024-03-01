7ccd59137dfeec19ac9366512c48a95f2a5a8e85
polibee <1244762327@qq.com>
2024-03-01 22:47:14 +0800

    找回密码成功消息提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -25,5 +27,15 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = RouteServiceProvider::HOME;
+    public function __construct()
+    {
+        $this->middleware('guest');
+
+    }
+    protected function sendResetResponse(Request $request, $response)
+    {
+        session()->flash('success','密码更新成功，您已成功登录！');
+        return redirect($this->redirectPath());
+    }
 }
