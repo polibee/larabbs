f1e62d9912bd02603fd9a2a7371c226630a3035e
polibee <1244762327@qq.com>
2024-03-17 21:22:13 +0800

    分类话题列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -10,7 +10,13 @@
 
         <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav">
+            <ul class="navbar-nav mr-auto">
+                
+                <li class="nav-item"><a class="nav-link active" href="{{route('topics.index')}}">话题</a></li>
+                <li class="nav-item"><a class="nav-link active" href="{{route('categories.show',1)}}">分享</a></li>
+                <li class="nav-item"><a class="nav-link active" href="{{route('categories.show',2)}}">教程</a></li>
+                <li class="nav-item"><a class="nav-link active" href="{{route('categories.show',3)}}">问答</a></li>
+                <li class="nav-item"><a class="nav-link active" href="{{route('categories.show',4)}}">公告</a></li>
 
             </ul>
 
